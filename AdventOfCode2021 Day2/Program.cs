138acd5b5054f1c68a863f4a3761768ba5e4bdfb
Beardshrimp <simonekranawetter@gmail.com>
2021-12-02 12:25:08 +0100

    Part 1 of day 2

--- AdventOfCode2021 Day2/Program.cs
+++ AdventOfCode2021 Day2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Get the input
+string[] input = File.ReadAllLines(@"C:\Users\Simone\Documents\WIN21\C-Sharp\AdventOfCode2021 Day1\AdventOfCode2021 Day2\input.txt");
+
+
+int forward = 0;
+int depth = 0;
+
+//Split the input into the movement command and the number, and calculate the result
+foreach (string line in input)
+{
+    string[] tokens = line.Split(' ');
+    string movement = tokens[0];
+    string numberString = tokens[1];
+
+    if (movement == "forward")
+    {
+        forward += int.Parse(numberString);
+    }
+    else if (movement == "down")
+    {
+        depth += int.Parse(numberString);
+    }
+    else if (movement == "up")
+    {
+        depth -= int.Parse(numberString); 
+    }
+    else
+    { //You never know if there's something you didn't expect and I don't know how to properly adress errors yet
+        Console.WriteLine("yo, you messed up!");
+    }
+}
+//Result for part 1
+Console.WriteLine(forward * depth);
